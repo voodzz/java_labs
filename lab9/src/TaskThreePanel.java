58c6ac473bdad46efa2f84828b6e44f525e11965
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2024-11-16 16:13:16 +0300

    lab9 done

--- lab9/src/TaskThreePanel.java
+++ lab9/src/TaskThreePanel.java
@@ -2,22 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TaskThreePanel extends JPanel {
-    final ImageIcon[] images = new ImageIcon[]{
-            new ImageIcon("a.png"),
-            new ImageIcon("b.png"),
-            new ImageIcon("c.png"),
-            new ImageIcon("d.png")
+    private ButtonGroup group;
+    private final ImageIcon[] images = new ImageIcon[]{
+            new ImageIcon("notSelected.png"),
+            new ImageIcon("selected.png"),
+            new ImageIcon("pressed.png"),
+            new ImageIcon("rollover.png")
     };
+
     TaskThreePanel() {
         super();
         setLayout(new GridLayout(2,2));
+        group = new ButtonGroup();
 
-        ButtonGroup group = new ButtonGroup();
         for (int i = 0; i < images.length; ++i) {
-            JRadioButton radioButton = new JRadioButton(images[3]);
-            radioButton.setPressedIcon(images[0]);
-            radioButton.setRolloverIcon(images[1]);
-            radioButton.setSelectedIcon(images[2]);
+            Image image = images[i].getImage();
+            Image scaledInstance = image.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale
+            images[i] = new ImageIcon(scaledInstance);
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            JRadioButton radioButton = new JRadioButton(images[0]);
+            radioButton.setText((i + 1) + "");
+            radioButton.setSelectedIcon(images[1]);
+            radioButton.setPressedIcon(images[2]);
+            radioButton.setRolloverIcon(images[3]);
+            radioButton.setFont(new Font("Arial", Font.PLAIN, 24));
             group.add(radioButton);
             add(radioButton);
         }
